d5f00f325ddb4016782d0ac8278ade8f403504fa
kosmoas <jokermaster5512@gmail.com>
2025-06-08 05:34:41 -0400

    added a logout feature

--- backflask.py
+++ backflask.py
@@ -1,6 +1,6 @@
 from flask import( Flask, render_template, jsonify, request, json,
                    flash, get_flashed_messages, session, redirect)
-import requests, datetime, entriesdb, os
+import requests, datetime, os
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from dotenv import load_dotenv
@@ -14,9 +14,13 @@ back.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 userbase = SQLAlchemy(back)
 class User(userbase.Model):
     id = userbase.Column('id',userbase.Integer, primary_key=True)
-    \
     username = userbase.Column('username',userbase.String(80), unique=True, nullable=False)
     password = userbase.Column('password',userbase.String(200), nullable=False)
+class journalent(userbase.Model):
+    id = userbase.Column('id',userbase.Integer, primary_key=True)
+    user_id = userbase.Column('User_id', userbase.Integer, nullable = False)
+    content = userbase.Column('Content', userbase.Text, nullable = False)
+    date = userbase.Column('Date', userbase.String(100), nullable = False)
 with back.app_context():
     userbase.create_all()
 def load_entries():
@@ -70,28 +74,37 @@ def login():
                flash('Sorry incorrect credentials')
                return redirect('/login')
      return render_template('login.html')
+@back.route('/logout')
+def logout():
+     session.pop('user_id', None)
+     flash('You have been logged out 🎈')
+     return redirect('/login')
 @back.route('/dashboard')
 def dashpage():
     user_id = session.get('user_id')
+    if not user_id:
+         flash('Sorry please login first')
+         redirect('/login')
     user = User.query.get(user_id)
-
-    entrielist = entriesdb.get_all_entries()
+    entrielist = journalent.query.filter_by(user_id = user_id).all()
     return render_template('dashboard.html', entries = entrielist, user_id = user.username)
 @back.route('/new', methods = ['POST','GET']) #looking out for post methods or get methos
 def new_page():
     entry = ''
+    if 'user_id' not in session:
+         flash('You must be logged in to create a message')
+         return redirect('/login')
     if request.method == 'POST':
         entry = request.form.get('journal')
         if entry:
-                entries = load_entries()
-                data = {
-                    'Date': datetime.datetime.now().strftime('%B %d, %Y at %I:%M %p'),
-                    'user': 'Anon',
-                    'entry': entry
-                }
-                entries.append(data)
-                entriesdb.appendtodb(data['user'], data['entry'], data['Date'])
-                save_entries(entries)
+
+                data = journalent(
+                    date = datetime.datetime.now().strftime('%B %d, %Y at %I:%M %p'),
+                    user_id =session['user_id'],
+                    content = entry
+                )
+                userbase.session.add(data)
+                userbase.session.commit()
                 flash("✅ Your journal was saved successfully!")
                 return redirect("/dashboard")
 
